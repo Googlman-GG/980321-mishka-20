a16e694a6e95ef0a54929de5fea253fe8638cf12
David <ovdienkodavid408@gmail.com>
2020-07-29 22:56:02 +0200

    update

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
-const navWrapper = document.querySelector('.main-nav__list'),
+const navWrapper = document.querySelector('.navigation-wrapp'),
   navToogler = document.querySelector('.main-nav__toggle');
 navToogler.addEventListener('click', function (event) {
   navWrapper.classList.toggle('main-nav__list--show');
