160e0b1d74b728a8b652f4f38a92c29a7498d662
David <ovdienkodavid408@gmail.com>
2020-08-18 15:00:38 +0200

    резиновая шапка

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,7 @@
-const navWrapper = document.querySelector('.navigation-wrapp'),
+const navWrappers = Array.prototype.slice.call(document.querySelectorAll('.main-nav__list')),
   navToogler = document.querySelector('.main-nav__toggle');
-navToogler.addEventListener('click', function (event) {
-  navWrapper.classList.toggle('main-nav__list--show');
+navToogler.addEventListener('click', function (_event) {
+  navWrappers.forEach(function(nav) {
+      nav.classList.toggle('main-nav__list--show');
+  })
 });
