1ff0e757af263e2175be70fb2ea5857d215a806e
David <ovdienkodavid408@gmail.com>
2020-09-14 05:21:42 +0200

    11

--- source/source/js/app.js
+++
@@ -1,7 +0,0 @@
-const navWrappers = Array.prototype.slice.call(document.querySelectorAll('.main-nav__list')),
-  navToogler = document.querySelector('.main-nav__toggle');
-navToogler.addEventListener('click', function (_event) {
-  navWrappers.forEach(function(nav) {
-      nav.classList.toggle('main-nav__list--show');
-  })
-});
