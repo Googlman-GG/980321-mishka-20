a9b3ff656202245b87851d0c4a872cba5e942282
David <ovdienkodavid408@gmail.com>
2020-09-16 12:06:02 +0200

    11

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,17 @@
-const navWrappers = Array.prototype.slice.call(document.querySelectorAll('.main-nav__list')),
-  navToogler = document.querySelector('.main-nav__toggle');
-navToogler.addEventListener('click', function (_event) {
-  navWrappers.forEach(function(nav) {
-      nav.classList.toggle('main-nav__list--show');
-  })
+const navWrappers = Array.prototype.slice.call(document.querySelectorAll('.main-nav')),
+    navToogler = document.querySelector('.page-header__toggle');
+navToogler.addEventListener('click', function(_event) {
+    navWrappers.forEach(function(nav) {
+        nav.classList.toggle('main-nav--show');
+    })
 });
+
+function toggleShow() {
+    navWrappers.forEach(function(nav) {
+        nav.classList.add('main-nav--nojs');
+    });
+}
+
+navToogler.addEventListener('click', toggleShow);
+
+toggleShow();
